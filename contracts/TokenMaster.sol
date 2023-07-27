9375d9dda522e1cafabf83c670f6c63f1bfb3192
johnie261 <njorogejohn261@gmail.com>
2023-07-27 18:10:47 +0300

    Withdraw ether to the wallet

--- contracts/TokenMaster.sol
+++ contracts/TokenMaster.sol
@@ -79,7 +79,11 @@ contract TokenMaster is ERC721 {
     return occasions[_id];
   }
 
-  
+  function withdraw() public onlyOwner {
+    (bool success, ) = owner.call{value: address(this).balance}("");
+    require(success);
+  }
+
   function getSeatsTaken(uint256 _id) public view returns (uint256[] memory) {
     return seatsTaken[_id];
   }
