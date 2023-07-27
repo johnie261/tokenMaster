e34d4eecadbd8734f43aaf61d3cf09c0363f4b31
johnie261 <njorogejohn261@gmail.com>
2023-07-27 12:07:43 +0300

    Create smart contracts

--- test/TokenMaster.js
+++ test/TokenMaster.js
@@ -1,5 +1,30 @@
 const { expect } = require("chai")
 
-describe("TokenMaster", () => {
+const NAME = "TokenMaster"
+const SYMBOL = "TM"
 
+describe("TokenMaster", () => {
+  let tokenMaster
+  let deployer, buyer
+
+  beforeEach(async () => {
+    [deployer, buyer] = await ethers.getSigners()
+    const TokenMaster = await ethers.getContractFactory("TokenMaster")
+    tokenMaster = await TokenMaster.deploy(NAME, SYMBOL)
+  })
+
+  describe("Deployment", () => {
+    it("Sets the name", async () => {
+      expect(await tokenMaster.name()).to.equal(NAME)
+    })
+
+    it("Sets the symbol", async () => {
+      expect(await tokenMaster.symbol()).to.equal(SYMBOL)
+    })
+
+    it("Sets the owner", async () => {
+      expect(await tokenMaster.owner()).to.equal(deployer.address)
+    })
+
+  })
 })
